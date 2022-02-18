b62b88aa31862eda346aa33e696ae3c3362ce6da
Janis Boegershausen (Pc) <janis.boegershausen@gmx.net>
2022-02-18 01:20:52 +0100

    Implemented "AddRow" and "AddColumn" to the Matrix class

--- Matrix.js
+++ Matrix.js
@@ -14,10 +14,10 @@ class Matrix {
     /** Crate an nxn identity-matrix. */
     static CreateIdentityMatrix(n) {
         var matrix = new Matrix(n, n);
-        for (let x = 0; x < n; x++) {
-            matrix[x] = [];
-            for (let y = 0; y < n; y++) {
-                matrix[x][y] = (x == y) ? 1 : 0;
+        for (let r = 0; r < n; r++) {
+            matrix[r] = [];
+            for (let c = 0; c < n; c++) {
+                matrix[r][c] = (r == c) ? 1 : 0;
             }
         }
         return matrix;
@@ -27,31 +27,46 @@ class Matrix {
     Clear() {
         this.value = [];
 
-        for (let x = 0; x < this.width; x++) {
-            this.value[x] = [];
-            for (let y = 0; y < this.height; y++) {
-                this.value[x][y] = 0;
+        for (let r = 0; r < this.height; r++) {
+            this.value[r] = [];
+            for (let c = 0; c < this.width; c++) {
+                this.value[r][c] = 0;
             }
         }
     }
 
     /** Set the entry at x,y to v. */
-    SetEntry(x, y, v) {
-        this.value[x][y] = v;
+    SetEntry(r, c, v) {
+        this.value[r][c] = v;
     }
 
     /** Get the entry at x,y. */
-    GetEntry(x, y) {
-        return this.value[x][y];
+    GetEntry(r, c) {
+        return this.value[r][c];
     }
 
     /** Fill each entry in the matrix with v. */
     Fill(v) {
-        for (let x = 0; x < this.width; x++) {
-            this.value[x] = [];
-            for (let y = 0; y < this.height; y++) {
-                this.value[x][y] = v;
+        for (let r = 0; r < this.height; r++) {
+            this.value[r] = [];
+            for (let c = 0; c < this.width; c++) {
+                this.value[r][c] = v;
             }
         }
+    }
+
+    AddRow() {
+        this.value.push([]);
+        for (let c = 0; c < this.width; c++) {
+            this.value[this.height][c] = 0;
+        }
+        this.height += 1;
+    }
+
+    AddCollumn() {
+        for (let r = 0; r < this.height; r++) {
+            this.value[r][this.width] = 0;
+        }
+        this.width += 1;
     }
 }
